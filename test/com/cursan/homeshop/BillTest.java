b813568a6ebdcb256ae3e5a59eb716fdc553ee03
Xsims <romeo.prosecco@gmail.com>
2018-08-20 15:33:08 +0200

    BillServlet display product and a form to add an address and choose the type of delivery http://localhost:8080/bill

--- test/com/cursan/homeshop/BillTest.java
+++ test/com/cursan/homeshop/BillTest.java
@@ -1,8 +1,8 @@
 package com.cursan.homeshop;
 
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class BillTest {
     private String output;
--- test/com/cursan/homeshop/RelayDeliveryTest.java
+++ test/com/cursan/homeshop/RelayDeliveryTest.java
@@ -1,7 +1,8 @@
 package com.cursan.homeshop;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class RelayDeliveryTest {
 
